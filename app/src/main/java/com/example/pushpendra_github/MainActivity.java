4b4dfa97c026918f1969209a62c7bdbf9b460e18
vishwas verma <114891824+Vishwas-verma5@users.noreply.github.com>
2023-06-12 16:23:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pushpendra_github/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.pushpendra_github;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
